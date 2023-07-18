aa25bf32e8152c27526ac0f6102c6a860ea623f6
Lorena Morais <lorenamorais@alunos.utfpr.edu.br>
2023-07-17 23:52:02 -0300

    feat: add AuthModule on app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
