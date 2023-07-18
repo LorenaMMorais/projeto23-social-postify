7fa37e333c8514625814887a917b137c29ca37b6
Lorena Morais <lorenamorais@alunos.utfpr.edu.br>
2023-07-18 00:27:00 -0300

    feat: add publications controller, service, module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
+import { PublicationsModule } from './publications/publications.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule],
+  imports: [PrismaModule, UsersModule, AuthModule, PublicationsModule],
   controllers: [],
   providers: [],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,18 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import * as bcrypt from "bcrypt";
+import { JwtService } from "@nestjs/jwt";
 import { AuthSigninDTO } from "./dto/auth-signin.dto";
 import { AuthSignupDTO } from "./dto/auth.signup.dto";
 import { UsersService } from "src/users/users.service";
 import { UserRepository } from "src/users/repository/users.repository";
+import { users } from "@prisma/client"; 
 
 @Injectable()
 export class AuthService {
     constructor(
         private readonly usersService: UsersService,
-        private readonly userRepository: UserRepository
+        private readonly userRepository: UserRepository,
+        private readonly jwtService: JwtService
     ) {}
 
     async signup(body: AuthSignupDTO) {
@@ -29,7 +32,19 @@ export class AuthService {
         return this.createToken(user);
     }
 
-    createToken(user: AuthSignupDTO) {
-        throw new Error('Method not implemented.');
+    createToken(user: users) {
+        const token = this.jwtService.sign(
+            {
+                name: user.name,
+                email: user.email
+            },
+            {
+                expiresIn: '7 days',
+                subject: String(user.id),
+                issuer: 'admin',
+                audience: 'users'
+            }
+        );
+        return { token };
     }
 }
